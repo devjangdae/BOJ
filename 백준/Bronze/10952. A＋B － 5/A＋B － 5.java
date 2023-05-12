822817ea8b499814b70671d6071cd18cbbe15684
Jangdae Seo <tjqkd0912@gmail.com>
2023-05-12 22:55:32 +0900

    [Bronze V] Title: A+B - 5, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder result = new StringBuilder();
+
+        boolean flag = true;
+
+        while (flag) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == 0 && b == 0) {
+                flag = false;
+            } else {
+                result.append(a + b).append("\n");
+            }
+        }
+
+        System.out.println(result);
+    }
+}
