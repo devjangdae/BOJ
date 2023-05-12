271b9a6b5190f13084b4c42a4d36fa4e748411a9
Jangdae Seo <tjqkd0912@gmail.com>
2023-05-12 22:30:11 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder result = new StringBuilder();
+
+        int loop = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < loop; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            result.append(a+b).append("\n");
+        }
+
+        System.out.println(result);
+    }
+}
