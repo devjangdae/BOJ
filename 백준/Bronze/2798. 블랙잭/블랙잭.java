7e807511879865d47407979ca4addb1b8dee5fa5
Jangdae Seo <tjqkd0912@gmail.com>
2023-06-08 14:27:17 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int MAX = Integer.parseInt(st.nextToken());
+        int sum = -1;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = i+1; j < N; j++) {
+                for (int k = j+1; k < N; k++) {
+                    int temp = arr[i] + arr[j] + arr[k];
+                    if (sum < temp && MAX >= temp) {
+                        sum = temp;
+                    }
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
