b5ed11223c617d249f3f5e915d97beb863c9eb27
JANGDAE SEO <tjqkd0912@gmail.com>
2023-06-15 22:08:35 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13828 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
