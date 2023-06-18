04cbf71b2d88a2e10866e0b00ca8e840680dd986
JANGDAE SEO <tjqkd0912@gmail.com>
2023-06-18 23:27:34 +0900

    [Bronze V] Title: 크냐?, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        boolean flag = true;
+        while (flag) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            if (N == 0 && M == 0) {
+                flag = false;
+            } else {
+                if(N > M){
+                    sb.append("Yes"+"\n");
+                } else {
+                    sb.append("No"+"\n");
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
