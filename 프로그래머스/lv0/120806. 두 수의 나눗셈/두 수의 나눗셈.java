ebc97172a786149eed8512f96dd49c8be93de7f8
JANGDAE SEO <tjqkd0912@gmail.com>
2023-07-14 12:03:29 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.22 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public double solution(double num1, double num2) {
+        double answer = 0;
+        answer = num1 / num2 * 1000;
+        answer = Math.floor(answer);
+        return answer;
+    }
+}
