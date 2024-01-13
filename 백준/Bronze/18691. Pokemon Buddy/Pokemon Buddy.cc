8327b390f125deca40306d7da7f13f166b17eaa1
JANGDAE SEO <tjqkd0912@gmail.com>
2024-01-13 21:32:27 +0900

    [Bronze IV] Title: Pokemon Buddy, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18691. Pokemon Buddy/Pokemon Buddy.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int caseCnt; cin >> caseCnt;
+
+  for (int i = 0; i < caseCnt; i++) {
+    int group, initialCandies, evolveCandies;
+    cin >> group >> initialCandies >> evolveCandies;
+
+    int requiredCandies = evolveCandies - initialCandies;
+
+    int requriedKm;
+    if (group == 1) requriedKm = requiredCandies;
+    else if (group == 2) requriedKm = 3 * requiredCandies;
+    else requriedKm = 5 * requiredCandies;
+
+    if (requriedKm <= 0) cout << "0\n";
+    else  cout << requriedKm << "\n";
+  }
+
+  return 0;
+}
