a3d732ce14e5d0fd2810548f927d7a72d03fb34c
JANGDAE SEO <tjqkd0912@gmail.com>
2024-10-25 09:13:10 +0900

    [Bronze III] Title: 암호 만들기, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21553. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string a, p; cin >> a >> p;
+
+  string b = p;
+  cout << b << "\n";
+
+  return 0;
+}
