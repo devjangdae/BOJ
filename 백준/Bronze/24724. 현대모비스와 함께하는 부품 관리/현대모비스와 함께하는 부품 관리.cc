a244671850cede8a8a9cdf657a6f6bda0ca77465
JANGDAE SEO <tjqkd0912@gmail.com>
2024-10-29 09:05:48 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T; cin >> T;
+	for (int t = 1; t <= T; t++) {
+		cout << "Material Management " << t << '\n' << "Classification ---- End!\n";
+	}
+}
