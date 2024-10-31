9984328214563a2566337ee4656cb6f86eb8d733
JANGDAE SEO <tjqkd0912@gmail.com>
2024-11-01 08:55:03 +0900

    [Bronze III] Title: Not A + B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23530. Not A ＋ B/Not A ＋ B.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int cntCase; cin >> cntCase;
+  for (int c = 0; c < cntCase; c++) {
+    int a, b; cin >> a >> b;
+    cout << 1 << "\n";
+  }
+
+  return 0;
+}
