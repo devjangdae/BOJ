fef83ad191c2dce4336b062af77823fe7a9919a2
JANGDAE <tjqkd0912@gmail.com>
2025-11-30 10:26:09 +0900

    [Bronze II] Title: 손익분기점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, c,ans;
+	cin >> a >> b >> c; 
+
+	if (b >= c)
+		cout << "-1";   
+	else {
+		ans = a / (c - b) + 1;
+		cout << ans;
+	}
+	
+}
