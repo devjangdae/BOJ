3902ccfbedf686347b9bf6e59b346fcc8bd13864
JANGDAE <tjqkd0912@gmail.com>
2025-12-09 15:02:44 +0900

    [Bronze II] Title: 좋은놈 나쁜놈, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4447. 좋은놈 나쁜놈/좋은놈 나쁜놈.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    int test;
+    cin>>test;
+    cin.ignore();
+    string str;
+ 
+    for(int i=0; i<test; i++){
+        getline(cin, str);
+ 
+        int cntg=0; 
+        int cntb=0; 
+ 
+        for(int i=0; i<str.length(); i++){
+            if(str[i]=='G'||str[i]=='g'){
+                cntg++;
+            }
+            else if(str[i]=='B'||str[i]=='b'){
+                cntb++;
+            }
+        }
+ 
+        if(cntg==cntb){
+                cout<<str<<" is "<<"NEUTRAL\n";
+            }
+            else if(cntg>cntb){
+                cout<<str<<" is "<<"GOOD\n";
+            } else {
+            cout<<str<<" is "<<"A BADDY\n";
+        }
+    }
+}
